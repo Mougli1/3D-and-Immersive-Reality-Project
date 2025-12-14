eea79eaa28dc44f16eea51a2ef014d63363fb6ef
Haylena <151937023+Haylena@users.noreply.github.com>
2025-12-14 18:40:21 +0100

    Ajout des poubelles (scripts, assets et scene)

---
+++ Project_test/Assets/Scripts/Dechet.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Dechet : MonoBehaviour
+{
+    public enum TypeDechet { Papier, Emballages, Verre, Organique }
+    public TypeDechet type;
+}
---
+++ Project_test/Assets/Scripts/TrashBinLidScript.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+
+public class TrashBinLidScript : MonoBehaviour
+{
+    public Transform lid;   // Objet du couvercle
+    public float openAngle = -70f;
+    public float speed = 4f;
+
+    private Quaternion closedRot;
+    private Quaternion openRot;
+
+    void Start()
+    {
+        //Rotation de base = fermé
+        closedRot = lid.localRotation;
+
+        //Rotation quand c’est ouvert
+        openRot = Quaternion.Euler(openAngle, 0, 0);
+    }
+
+    public void OpenLid()
+    {
+        Debug.Log("OUVERTURE DU COUVERCLE !");
+        StopAllCoroutines();
+        StartCoroutine(OpenClose());
+    }
+
+    IEnumerator OpenClose()
+    {
+        float t = 0f;
+
+        //Ouvre
+        while (t < 1)
+        {
+            t += Time.deltaTime * speed;
+            lid.localRotation = Quaternion.Slerp(closedRot, openRot, t);
+            yield return null;
+        }
+
+        // Petite pause
+        yield return new WaitForSeconds(0.5f);
+
+        // Referme
+        t = 0f;
+        while (t < 1)
+        {
+            t += Time.deltaTime * speed;
+            lid.localRotation = Quaternion.Slerp(openRot, closedRot, t);
+            yield return null;
+        }
+    }
+}
